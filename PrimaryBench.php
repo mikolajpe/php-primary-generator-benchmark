a157988a78798f81a00998da4081918077f2879a
Mikolaj Przybysz <mikolaj.przybysz@home24.de>
2016-06-16 14:17:52 +0200

    primary benchmark

---
+++ PrimaryBench.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * Class GeneratorBench
+ *
+ * @Iterations(100)
+ * @Revs(200)
+ */
+final class PrimaryBench
+{
+    function isPrime($num) {
+        //1 is not prime. See: http://en.wikipedia.org/wiki/Prime_number#Primality_of_one
+        if($num == 1)
+            return false;
+
+        //2 is prime (the only even number that is prime)
+        if($num == 2)
+            return true;
+
+        /**
+         * if the number is divisible by two, then it's not prime and it's no longer
+         * needed to check other even numbers
+         */
+        if($num % 2 == 0) {
+            return false;
+        }
+
+        /**
+         * Checks the odd numbers. If any of them is a factor, then it returns false.
+         * The sqrt can be an aproximation, hence just for the sake of
+         * security, one rounds it to the next highest integer value.
+         */
+        for($i = 3; $i <= ceil(sqrt($num)); $i = $i + 2) {
+            if($num % $i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @Iterations(1000)
+     */
+    public function benchPrimeNormal()
+    {
+        $self = $this;
+        $tgen = function($v) use ($self) {
+            while (!$self->isPrime(++$v));
+            return $v;
+        };
+
+        $i = 7;
+        for ($a = 0; $a < 100; $a++) {
+            $i = $tgen($i);
+        }
+    }
+
+    /**
+     * @Iterations(1000)
+     */
+    public function benchPrimeOldNormalWhile()
+    {
+        $self = $this;
+        $tgen = function($v) use ($self) {
+            while (true) {
+                if ($self->isPrime($v)) {
+                    return $v;
+                }
+                $v++;
+            };
+        };
+
+        $i = 7;
+        for ($a = 0; $a < 100; $a++) {
+            $i = $tgen($i);
+        }
+    }
+
+    /**
+     * @Iterations(1000)
+     */
+    public function benchPrimeOldNormalFor()
+    {
+        $self = $this;
+        $tgen = function($v) use ($self) {
+            for(;;$v++) {
+                if ($self->isPrime($v)) {
+                    return $v;
+                }
+            };
+        };
+
+        $i = 7;
+        for ($a = 0; $a < 100; $a++) {
+            $i = $tgen($i);
+        }
+    }
+
+    /**
+     * @Iterations(1000)
+     */
+    public function benchPrimeOldNormalGoto()
+    {
+        $self = $this;
+        $tgen = function($v) use ($self) {
+            start:
+            if ($self->isPrime($v)) {
+                return $v;
+            }
+            goto start;
+        };
+
+        $i = 7;
+        for ($a = 0; $a < 100; $a++) {
+            $i = $tgen($i);
+        }
+    }
+
+
+    /**
+     * @Iterations(1000)
+     */
+    public function benchPrimeGenerator()
+    {
+        $self = $this;
+        $tgen = function($v) use ($self) {
+            while (!$self->isPrime(++$v));
+            yield $v;
+        };
+
+
+        $i = 7;
+        $gen = $tgen($i);
+        for ($a = 0; $a < 100; $a++) {
+            $b = $gen->next();
+        }
+
+    }
+
+    /**
+     * @Iterations(1000)
+     */
+    public function benchPrimeGeneratorIf()
+    {
+        $self = $this;
+        $tgen = function($v) use ($self) {
+            while (true) {
+                if($self->isPrime(++$v)) {
+                    yield $v;
+                }
+            };
+        };
+
+
+        $i = 7;
+        $gen = $tgen($i);
+        for ($a = 0; $a < 100; $a++) {
+            $b = $gen->next();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # php-primary-generator
+
+```
+composer install
+./vendor/bin/phpbench run PrimaryBench.php --report=default
+```
+
+```
+PhpBench 0.11-dev (@git_sha@). Running benchmarks.
+Using configuration file: /home/mprzybysz/work/GeneratorBenchmark/phpbench.json
+
+\PrimaryBench
+
+    benchPrimeNormal              I999 P0 	[μ Mo]/r: 365.044 360.656 (μs) 	[μSD μRSD]/r: 10.521μs 2.88%
+    benchPrimeOldNormalWhile      I999 P0 	[μ Mo]/r: 77.344 76.093 (μs) 	[μSD μRSD]/r: 3.370μs 4.36%
+    benchPrimeOldNormalFor        I999 P0 	[μ Mo]/r: 77.212 75.721 (μs) 	[μSD μRSD]/r: 3.068μs 3.97%
+    benchPrimeOldNormalGoto       I999 P0 	[μ Mo]/r: 76.721 75.384 (μs) 	[μSD μRSD]/r: 3.232μs 4.21%
+    benchPrimeGenerator           I999 P0 	[μ Mo]/r: 13.199 12.488 (μs) 	[μSD μRSD]/r: 1.559μs 11.81%
+    benchPrimeGeneratorIf         I999 P0 	[μ Mo]/r: 409.301 404.286 (μs) 	[μSD μRSD]/r: 14.352μs 3.51%
+
+6 subjects, 6,000 iterations, 1,200 revs, 0 rejects
+(best [mean mode] worst) = 11.375 [169.803 167.438] 22.750 (μs)
+⅀T: 1,018,820.180μs μSD/r 6.017μs μRSD/r: 5.124%
+suite: 133a068e31a088ed17154e96051f76b14f9803eb, date: 2016-06-16, stime: 14:06:16
+```
